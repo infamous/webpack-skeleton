f93d0cb9de8f06865fbdcab953d7902af286b683
Joseph Orbegoso Pea <joe@trusktr.io>
2015-10-06 07:22:38 -0700

    Basic top-level ideal usage example.

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,22 @@
 var webpack = require('webpack')
+var path = require('path')
+
 module.exports = {
     entry: "./src/app.js",
     output: {
         path: './public',
-        filename: "app.js"
+        filename: "app.js",
     },
     module: {
         loaders: [
-            { test: /\.css$/, loader: "style!css" }
-        ]
+            { test: /\.jsx?$/,  loader: 'babel', exclude: /node_modules/ },
+            { test: /\.css$/, loader: "style!css" },
+        ],
     },
     resolve: {
+        fallback: path.join(__dirname, "node_modules"),
         alias: {
             famous: 'famous/src'
-        }
-    }
+        },
+    },
 }
